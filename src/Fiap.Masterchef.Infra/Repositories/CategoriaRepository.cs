2b40c3ffdd1aa500d1b1f387302585ccc7c3944d
Wellington <wellington@wton.com.br>
2017-09-12 21:22:33 -0300

    criação da camada infra e instalação do entity framework

---
+++ src/Fiap.Masterchef.Infra/Repositories/CategoriaRepository.cs
@@ -0,0 +1,14 @@
+﻿using Fiap.Masterchef.Core.Application.ViewModels;
+using Fiap.Masterchef.Core.Repositories;
+using System.Collections.Generic;
+
+namespace Fiap.Masterchef.Infra.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        public IEnumerable<CategoriaViewModel> ObterCategorias()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Fiap.Masterchef.Infra/Repositories/ReceitaRepository.cs
@@ -0,0 +1,24 @@
+﻿using Fiap.Masterchef.Core.Application.ViewModels;
+using Fiap.Masterchef.Core.Repositories;
+using System.Collections.Generic;
+
+namespace Fiap.Masterchef.Infra.Repositories
+{
+    public class ReceitaRepository : IReceitaRepository
+    {
+        public IEnumerable<VitrineViewModel> ObterFavoritas()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<VitrineViewModel> ObterReceitas()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<VitrineViewModel> ObterReceitas(string termo)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
