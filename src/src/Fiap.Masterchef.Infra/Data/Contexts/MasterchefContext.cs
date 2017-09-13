6961bbe2c69ef366b2d010f7018e8cabd195afa3
Wellington <wellington@wton.com.br>
2017-09-12 21:02:33 -0300

    criação da camada infra e instalação do entity framework

---
+++ src/src/Fiap.Masterchef.Infra/Data/Contexts/MasterchefContext.cs
@@ -0,0 +1,24 @@
+﻿using Fiap.Masterchef.Core;
+using Fiap.Masterchef.Infra.Data.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiap.Masterchef.Infra.Data.Contexts
+{
+    public class MasterchefContext : DbContext
+    {
+        public MasterchefContext(DbContextOptions<MasterchefContext> options)
+            :base(options)
+        { }
+        public DbSet<Receita> Receitas { get; set; }
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new ReceitaMap());
+            modelBuilder.ApplyConfiguration(new CategoriaMap());
+        }
+    }
+}
---
+++ src/src/Fiap.Masterchef.Infra/Data/Mappings/ReceitaMap.cs
@@ -0,0 +1,51 @@
+﻿using Fiap.Masterchef.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Fiap.Masterchef.Infra.Data.Mappings
+{
+    public class ReceitaMap : IEntityTypeConfiguration<Receita>
+    {
+        public void Configure(EntityTypeBuilder<Receita> builder)
+        {
+            builder.ToTable("Receita");
+
+            builder.HasKey(r => r.Id);
+
+            builder.Property(r => r.Titulo)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(r => r.Descricao)
+               .HasMaxLength(150)
+               .IsRequired();
+
+            builder.Property(r => r.Ingredientes)
+               .HasMaxLength(500)
+               .IsRequired();
+
+            builder.Property(r => r.Preparo)
+               .HasMaxLength(1000)
+               .IsRequired();
+
+            builder.Property(r => r.Foto)
+               .HasMaxLength(100)
+               .IsRequired();
+
+            builder.Property(r => r.Tags)
+               .HasMaxLength(200);
+
+            builder.Property(r => r.Favorita)
+               .IsRequired();
+
+            builder.Property(r => r.TempoPreparo)
+               .IsRequired();
+
+            builder.Property(r => r.CategoriaId)
+               .IsRequired();
+
+            builder.HasOne(r => r.Categoria)
+                .WithMany(r => r.Receitas);
+        }
+    }
+}
