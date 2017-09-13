c60e96cae01697a7582e6f1d6e0ad9ebc8d83083
Ariel Cardoso <arielccardoso@live.com>
2017-09-12 21:33:44 -0300

    Implementação da classe ReceitaApplicationService.

--- src/Fiap.Masterchef.Web/Startup.cs
+++ src/Fiap.Masterchef.Web/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,10 +28,6 @@ namespace Fiap.Masterchef.Web
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
 
             app.UseStaticFiles();
 
@@ -43,7 +35,7 @@ namespace Fiap.Masterchef.Web
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Receitas}/{action=Index}/{id?}");
             });
         }
     }
