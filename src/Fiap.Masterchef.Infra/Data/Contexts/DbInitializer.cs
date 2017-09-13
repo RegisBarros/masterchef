ec9c82ce0a8a601f9325b8cf2168a20bbeedec4d
Reginaldo Alves de Barros <r21regis@hotmail.com>
2017-09-12 22:44:35 -0300

    Inicializar categorias

---
+++ src/Fiap.Masterchef.Infra/Data/Contexts/DbInitializer.cs
@@ -0,0 +1,33 @@
+﻿using Fiap.Masterchef.Core;
+using System.Linq;
+
+namespace Fiap.Masterchef.Infra.Data.Contexts
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(MasterchefContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Categorias.Any())
+                return;
+
+            var categorias = new Categoria[]
+            {
+                new Categoria("Aves", "Receitas de aves"),
+                new Categoria("Bebidas", "Receitas de bebidas"),
+                new Categoria("Bolos e Tortas", "Receitas de bolos e tortas"),
+                new Categoria("Carnes", "Receitas de carnes"),
+                new Categoria("Doces e Sobremesas", "Receitas de doces e sobremesas"),
+                new Categoria("Lanches", "Receitas de lanches"),
+                new Categoria("Massas", "Receitas de massas"),
+                new Categoria("Saladas e Molhos", "Receitas de saladas e molhos"),
+                new Categoria("Sopas", "Receitas de sopas")
+            };
+
+            context.Categorias.AddRange(categorias);
+
+            context.SaveChanges();
+        }
+    }
+}
